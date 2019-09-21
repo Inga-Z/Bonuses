4bde32f9fec8a20ffb3c96db56d4ebbd63ba5ba2
Student <student@localhost>
2019-09-21 10:56:32 +0300

    Вариант со 100% оплаты бонусам

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('worked');
+
+const purchaseAmount = 1000;
+const bonuses = 2000;
+
+let total;
+if (purchaseAmount <= bonuses){
+    total = 0;
+} else {
+    total = purchaseAmount - bonuses;
+}
+
+
+console.log(total);
